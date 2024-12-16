7d4a2005662c4b6fd03b5a2285d5352dd9c6ee0c
Muhammed Ali Aktağ <157128004+Mali-aktag@users.noreply.github.com>
2024-12-16 23:14:24 +0300

    Initial commit

---
+++ MovieMicroservice/src/main/java/com/example/MovieMicroservice/model/Movie.java
@@ -0,0 +1,32 @@
+package com.example.MovieMicroservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+	
+	@Id
+	@generatedvalue
+	private Long id;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
